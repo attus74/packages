557d9cbe3049f7810f5e5ac810617a507af81593
Attila Németh <attila.nemeth@ubgnet.de>
2021-01-08 11:23:07 +0100

    No DEV Versions

--- src/Entity/ComposerProject.php
+++ src/Entity/ComposerProject.php
@@ -162,7 +162,7 @@ class ComposerProject extends ContentEntityBase implements ComposerProjectInterf
     $versions = [];
     if ($this->get('project_packages')->isEmpty()) {
       $versions['1.0.0'] = '1.0.0';
-      $versions['1.0-dev'] = '1.0-dev';
+//      $versions['1.0-dev'] = '1.0-dev';
     }
     else {
       $existing = [];
@@ -171,6 +171,9 @@ class ComposerProject extends ContentEntityBase implements ComposerProjectInterf
         $v = $item->entity->get('version')->first()->get('value')->getvalue();
         if (preg_match('/^([0-9]+)\.([0-9]+)(\.|\-)(.*?)$/', $v, $matches)) {
           $existing[$matches[1]][$matches[2]][$matches[4]] = TRUE;
+        }
+        elseif (preg_match('/^([0-9]+)\.([0-9]+)$/', $v, $matches)) {
+          $existing[$matches[1]][$matches[2]][0] = TRUE;
         }
       }
       ksort($existing);
@@ -193,15 +196,15 @@ class ComposerProject extends ContentEntityBase implements ComposerProjectInterf
           }
         }
         $versions[$major . '.' . ($maxMinor + 1) . '.0'] = $major . '.' . ($maxMinor + 1) . '.0';
-        $versions[$major . '.' . ($maxMinor) . '-dev'] = $major . '.' . ($maxMinor) . '-dev';
-        $versions[$major . '.' . ($maxMinor) . '-alpha'] = $major . '.' . ($maxMinor) . '-alpha';
+//        $versions[$major . '.' . ($maxMinor) . '-dev'] = $major . '.' . ($maxMinor) . '-dev';
+//        $versions[$major . '.' . ($maxMinor) . '-alpha'] = $major . '.' . ($maxMinor) . '-alpha';
         $versions[$major . '.' . ($maxMinor) . '.' . ($maxPatch + 1)] = $major . '.' . ($maxMinor) . '.' . ($maxPatch + 1);
-        $versions[$major . '.' . ($maxMinor + 1) . '-dev'] = $major . '.' . ($maxMinor + 1) . '-dev';
-        $versions[$major . '.' . ($maxMinor + 1) . '-alpha'] = $major . '.' . ($maxMinor + 1) . '-alpha';
+//        $versions[$major . '.' . ($maxMinor + 1) . '-dev'] = $major . '.' . ($maxMinor + 1) . '-dev';
+//        $versions[$major . '.' . ($maxMinor + 1) . '-alpha'] = $major . '.' . ($maxMinor + 1) . '-alpha';
       }
       $versions[($maxMajor + 1) . '.0.0'] = ($maxMajor + 1) . '.0.0';
-      $versions[($maxMajor + 1) . '.0-dev'] = ($maxMajor + 1) . '.0-dev';
-      $versions[($maxMajor + 1) . '.0-alpha'] = ($maxMajor + 1) . '.0-alpha';
+//      $versions[($maxMajor + 1) . '.0-dev'] = ($maxMajor + 1) . '.0-dev';
+//      $versions[($maxMajor + 1) . '.0-alpha'] = ($maxMajor + 1) . '.0-alpha';
     }
     ksort($versions);
     return $versions;
--- src/PackageManager.php
+++ src/PackageManager.php
@@ -48,12 +48,6 @@ class PackageManager {
     }
   }
   
-  public function pullUbg(): void
-  {
-    $tempPath = $this->_getRealTempDir();
-    
-  }
-  
   /**
    * Create and save a ZIP File
    */
@@ -66,7 +60,7 @@ class PackageManager {
     $zipUri = \Drupal::service('file_system')->realpath($zipDir . '/' . $zipFileName);
     $zip = new \ZipArchive; 
     $dir = $this->_getRealTempDir();
-    if ($zip ->open($zipUri, \ZipArchive::CREATE ) === TRUE) {
+    if ($zip->open($zipUri, \ZipArchive::CREATE ) === TRUE) {
       $this->_addFolderToZip($zip, $dir);
       $zip->close(); 
     }
@@ -204,7 +198,7 @@ class PackageManager {
    * Absolute Path of Temporary Directory
    * @return string
    */
-  private function _getRealTempDir(): string
+  protected function _getRealTempDir(): string
   {
     $dir = $this->_getTempDir();
     $result = \Drupal::service('file_system')
